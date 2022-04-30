7146a72db15ff8752ff6377e0e09c90aeb46756a
Ulrich Schmidt <104643852+uncsco@users.noreply.github.com>
2022-04-30 00:21:28 +0000

    Update: setup.cfg

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="realpython-reader-WIP",
-    version="0.1.0",
+    version="0.1.1",
     description="My description",
     long_description=README,
     long_description_content_type="text/markdown",
