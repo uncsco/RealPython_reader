690a2458b38ceb45edee12434ea7954e21da336e
Ulrich Schmidt <104643852+uncsco@users.noreply.github.com>
2022-04-30 00:34:08 +0000

    Version: 0.1.2

--- reader/__init__.py
+++ reader/__init__.py
@@ -1,2 +1,4 @@
+__version__ = "0.1.2"
+
 def hello(s: str) -> str:
-    return f'Hello... {s}!'
+    return f'Hello, {s}!!'
--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
-    name="realpython-reader-WIP",
-    version="0.1.1",
+    name="realpython-reader-wip",
+    version="0.1.2",
     description="My description",
     long_description=README,
     long_description_content_type="text/markdown",
