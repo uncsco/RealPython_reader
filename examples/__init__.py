e59cae1b39572465c7c562a97baa74fafabed5c5
Ulrich Schmidt <104643852+uncsco@users.noreply.github.com>
2022-04-30 17:51:54 +0000

    Tidy: `examples/`

---
+++ examples/__init__.py
@@ -0,0 +1,3 @@
+#// Based on: https://github.com/tiangolo/sqlmodel/blob/main/sqlmodel/__init__.py
+from . import eg_hello
+from . import eg_rich
--- examples_run.py
+++ examples_run.py
@@ -1,3 +1,4 @@
-import examples.hello
+#import examples.eg_hello
+#import examples.eg_rich
 
-import examples.eg_rich
+import examples
