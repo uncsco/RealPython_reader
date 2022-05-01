8da42f863c09a8853a9f817bbee23d44f95992b5
Ulrich Schmidt <104643852+uncsco@users.noreply.github.com>
2022-05-01 14:08:20 +0000

    [mypkg.std ]Update: __init__.py

--- examples/std/csv.py
+++ examples/std/csv.py
@@ -1,3 +1,8 @@
-from mypkg.std.csv import from_csv
+from mypkg.std.csv import from_csv as from_csv_000
+print(from_csv_000)
 
+
+from mypkg.std import from_csv
 print(from_csv)
+
+print(id(from_csv_000) == id(from_csv))
