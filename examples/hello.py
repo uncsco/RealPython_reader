117e96bcfe1a4d7451c125cd549f4c56b191bc0e
Ulrich Schmidt <104643852+uncsco@users.noreply.github.com>
2022-05-01 19:05:45 +0000

    [mypkg.hello] Add `_filter()`

--- examples/hello.py
+++ examples/hello.py
@@ -1,4 +1,5 @@
 import mypkg
 
 print(mypkg.__version__)
-print(mypkg.hello('Bob'))
+print(mypkg.hello('Amy'))
+print(mypkg.hello('Barbara', do_filter=True))
--- mypkg/hello.py
+++ mypkg/hello.py
@@ -2,10 +2,19 @@ __all__ = [
     'hello'
 ]
 
-def hello(s: str) -> str:
+def _filter(s: str) -> str:
+    """Return first 6 chars
+
+    - [ ] Can be *redefined*
+    """
+    return s[0:6]
+
+def hello(s: str, do_filter=False ) -> str:
     """Print hello...
 
     >>> hello('Amy')
     'Hello, Amy!!'
+    >>> hello('Barbara', do_filter=True)
+    'Hello, Barbar!!'
     """
-    return f'Hello, {s}!!'
+    return f'Hello, {_filter(s) if do_filter else s}!!'
