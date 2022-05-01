85d21fb5a28a8221d994344d17f3cc3242f8ec36
Ulrich Schmidt <104643852+uncsco@users.noreply.github.com>
2022-05-01 19:16:44 +0000

    {mypkg.__init__.py} ALLOW `hello._filter` to be redefined

--- examples/hello.py
+++ examples/hello.py
@@ -1,5 +1,20 @@
+# OKAY
+'''
 import mypkg
 
 print(mypkg.__version__)
 print(mypkg.hello('Amy'))
 print(mypkg.hello('Barbara', do_filter=True))
+'''
+
+from mypkg import hello
+
+def my_filter(s):
+    return s
+
+print(hello.hello('Amy'))
+print(hello.hello('Barbara', do_filter=True))
+
+# REDEFINE
+hello._filter = my_filter
+print(hello.hello('Barbara', do_filter=True))
--- mypkg/__init__.py
+++ mypkg/__init__.py
@@ -1,3 +1,3 @@
 __version__ = "0.1.9"
 
-from .hello import *
+#from .hello import *
