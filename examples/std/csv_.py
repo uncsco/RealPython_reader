10d0d7c2e1b62c13a1a2321d021bbc2532d9a0a5
Ulrich Schmidt <104643852+uncsco@users.noreply.github.com>
2022-05-01 20:13:55 +0000

    [std.csv_] FIX filenames...

--- examples/std/csv_.py
+++ examples/std/csv_.py
@@ -1,8 +1,9 @@
-from mypkg.std.csv import from_csv as from_csv_000
+from mypkg.std.csv_ import from_csv as from_csv_000
 print(from_csv_000)
 
-
+'''
 from mypkg.std import from_csv
 print(from_csv)
 
 print(id(from_csv_000) == id(from_csv))
+'''
