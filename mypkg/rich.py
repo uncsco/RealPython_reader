3d770d57b636caab56efa92b3aac2b2203d11db2
Ulrich Schmidt <104643852+uncsco@users.noreply.github.com>
2022-05-02 00:14:21 +0000

    Update: rich.py

--- mypkg/rich.py
+++ mypkg/rich.py
@@ -2,5 +2,18 @@ try:
     from rich import print as rprint
     from rich import print_json as rprint_json
 except ImportError:
-    def rprint(s): print(s)
-    def rprint_json(s): print(s)
+    def rprint(s) -> None:
+        print(s)
+    def rprint_json(s) ->None:
+        print(s)
+
+'''
+MYPY:
+
+```
+error: All conditional function variants must have identical signatures
+```
+
+- rprint() should have *same* signature as rich.print
+- rprint_json() ...
+'''
